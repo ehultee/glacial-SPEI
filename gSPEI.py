205cbe78667627c6e45716ade4a95119bdab03d3
Lizz <ehultee@umich.edu>
2019-09-26 12:09:46 -0400

    Generalize plot_basin_runmean
    Previous function definition used implicit default of plotting 'diff' (difference of with/without runoff SPEI).  Added this as an explicit default variable so that the same function can be used for nice running mean plots of W/N runoff values.

--- gSPEI.py
+++ gSPEI.py
@@ -18,18 +18,19 @@ basin_names = ['INDUS','TARIM','BRAHMAPUTRA','ARAL SEA','COPPER','GANGES','YUKON
 'KALIXAELVEN','MAGDALENA','DRAMSELV','COLVILLE']
 
 
-def plot_basin_runmean(basin_id, permodel_dict, window_yrs=30, cmap_name='viridis', show_plot=True, save_plot=False):
+def plot_basin_runmean(basin_id, permodel_dict, which='diff', window_yrs=30, cmap_name='viridis', show_plot=True, save_plot=False):
     """Make a plot of running mean difference in SPEI for a given basin, comparing across models.
     Arguments:
         basin_id: integer, index of basin in the standard list "basin_names"
-        permodel_dict: dictionary storing SPEI per model, with the structure dict[modelname]['diff'][basinname] = basin difference in SPEI for this model
+        permodel_dict: dictionary storing SPEI per model, with the structure dict[modelname]['diff'/'WRunoff'/'NRunoff'][basinname] = basin difference in SPEI for this model
+        which: string identifying 'WRunoff' (with glacial runoff), 'NRunoff' (no runoff), or 'diff' (their difference)
         window_yrs: number of years to consider in running average.  Default 30
         cmap_name: name of matplotlib colormap from which to select line colors. Default 'viridis'
         show_plot: Boolean, whether to show the resulting plot.  Default True
         save_plot: Boolean, whether to save the plot in the working directory.  Default False
     """
     window_size = 12 * window_yrs # size of window given monthly data
-    basin_runavg_bymodel = [np.convolve(permodel_dict[m]['diff'][basin_id], np.ones((window_size,))/window_size, mode='valid') for m in model_names] #compute running means
+    basin_runavg_bymodel = [np.convolve(permodel_dict[m][which][basin_id], np.ones((window_size,))/window_size, mode='valid') for m in model_names] #compute running means
     colors = cm.get_cmap(cmap_name)(np.linspace(0, 1, num=len(model_names)))
     styles = ('-',':')
     plt.figure('{} year running average glacial effect, {} basin'.format(window_yrs, basin_names[basin_id]))
