cc13eeec1c8125291f6305b697da72f3a5461ab2
Lizz Ultee [air] <ehultee@umich.edu>
2020-05-09 20:53:35 -0400

    Create supplementary_figs.py
    New script to automatically composite supplementary figures, previously composited by hand.  Re-generate manuscript supplement to include new label styling, separate legend, shading on 1900-1995.

---
+++ supplementary_figs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 18:59:37 2020
+
+@author: lizz
+
+## Script generation of supplementary figures to avoid manual compositing
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Figure S2: isolated glacial effect in 56 basins, alphabetised
+
+## Alphabetise records
+basin_names = ['INDUS','TARIM','BRAHMAPUTRA','ARAL SEA','COPPER','GANGES','YUKON','ALSEK','SUSITNA','BALKHASH','STIKINE','SANTA CRUZ',
+'FRASER','BAKER','YANGTZE','SALWEEN','COLUMBIA','ISSYK-KUL','AMAZON','COLORADO','TAKU','MACKENZIE','NASS','THJORSA','JOEKULSA A F.',
+'KUSKOKWIM','RHONE','SKEENA','OB','OELFUSA','MEKONG','DANUBE','NELSON RIVER','PO','KAMCHATKA','RHINE','GLOMA','HUANG HE','INDIGIRKA',
+'LULE','RAPEL','SANTA','SKAGIT','KUBAN','TITICACA','NUSHAGAK','BIOBIO','IRRAWADDY','NEGRO','MAJES','CLUTHA','DAULE-VINCES',
+'KALIXAELVEN','MAGDALENA','DRAMSELV','COLVILLE']
+basin_ids_raw = np.arange(len(basin_names))
+basin_ids_sorted = sorted(zip(basin_names, basin_ids_raw), key=lambda x: x[0])
+
+
+## Plot
+page_chunks = np.arange(0, len(basin_names)+1, step=8)
+for i in range(len(page_chunks)):
+    chunk = range(page_chunks[i], page_chunks[i+1])
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2, sharex=True, figsize=(6, 12))
+    axs = np.asarray(((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8))).ravel()
+    for j, k in enumerate(chunk):
+        gSPEI.plot_basin_runmean(basin_id=basin_ids_sorted[k][1], permodel_dict=SPEI_by_model, show_labels=False, show_plot=False, save_plot=False, ax=axs[j])
+    ax1.legend(loc='best')
+    plt.show()
