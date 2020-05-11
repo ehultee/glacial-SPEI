5d3195931468c5e3beb3ef64995496608d6651ef
Lizz Ultee [air] <ehultee@umich.edu>
2020-05-10 22:15:14 -0400

    Output all supplementary figs in page-size chunks

--- supplementary_figs.py
+++ supplementary_figs.py
@@ -26,9 +26,20 @@ basin_ids_sorted = sorted(zip(basin_names, basin_ids_raw), key=lambda x: x[0])
 page_chunks = np.arange(0, len(basin_names)+1, step=8)
 for i in range(len(page_chunks)):
     chunk = range(page_chunks[i], page_chunks[i+1])
-    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2, sharex=True, figsize=(6, 12))
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2, sharex=True, figsize=(6, 16))
     axs = np.asarray(((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8))).ravel()
     for j, k in enumerate(chunk):
         gSPEI.plot_basin_runmean(basin_id=basin_ids_sorted[k][1], permodel_dict=SPEI_by_model, show_labels=False, show_plot=False, save_plot=False, ax=axs[j])
-    ax1.legend(loc='best')
+        axs[j].tick_params(axis='both', labelsize=10) #small labels appear large when scaled up to page size
+    #ax1.legend(loc='best')
+    plt.show()
+
+for i in range(len(page_chunks)):
+    chunk = range(page_chunks[i], page_chunks[i+1])
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2, sharex=True, figsize=(6, 16))
+    axs = np.asarray(((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8))).ravel()
+    for j, k in enumerate(chunk):
+        gSPEI.plot_basin_runvar(basin_id=basin_ids_sorted[k][1], permodel_dict=SPEI_by_model, show_labels=False, show_plot=False, save_plot=False, ax=axs[j])
+        axs[j].tick_params(axis='both', labelsize=10) #small labels appear large when scaled up to page size
+    #ax1.legend(loc='best')
     plt.show()
