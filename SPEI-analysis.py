ffd598339ae846b97dae06510789a741ef075f1e
Lizz Ultee [air] <ehultee@umich.edu>
2020-06-29 11:47:49 -0400

    Remove unused code
    Remove code used for preliminary analysis on shifts in mean SPEI over time.  This analysis was not pursued for the manuscript; can be re-introduced later if needed.

--- SPEI-analysis.py
+++ SPEI-analysis.py
@@ -38,60 +38,9 @@ for i in (1, 4, 26, -7): #plot the basins shown in manuscript main text
     plot_basin_runmean(basin_id=i, permodel_dict=SPEI_by_model, show_labels=False, show_plot=False, save_plot=True)
     plot_basin_runvar(basin_id=i, permodel_dict=SPEI_by_model, show_labels=False, show_plot=False, save_plot=True)
 
-
 # for i in range(len(basin_names)): #plot all
 #     plot_basin_runmean(basin_id=i, permodel_dict=SPEI_by_model, save_plot=True, show_plot=False)
 #     plot_basin_runvar(basin_id=i, permodel_dict=SPEI_by_model, save_plot=True, show_plot=False)
-
-
-# ## 30-yr period means and variance
-# modelmeans_1950_1980 = {m: [] for m in modelnames} #dictionary indexed by model name
-# modelvar_1950_1980 = {m: [] for m in modelnames}
-# modelmeans_2070_2100 = {m: [] for m in modelnames}
-# modelvar_2070_2100 = {m: [] for m in modelnames}
-# mean_shifts = {m: [] for m in modelnames}
-# var_shifts = {m: [] for m in modelnames}
-
-# for m in modelnames:
-#     means_i = [np.nanmean(SPEI_by_model[m]['diff'][j][600:971]) for j in range(len(basin_names))]
-#     var_i = [np.nanvar(SPEI_by_model[m]['diff'][j][600:971]) for j in range(len(basin_names))]
-#     means_f = [np.nanmean(SPEI_by_model[m]['diff'][j][2039:2410]) for j in range(len(basin_names))]
-#     var_f = [np.nanvar(SPEI_by_model[m]['diff'][j][2039:2410]) for j in range(len(basin_names))]
-#     modelmeans_1950_1980[m] = means_i
-#     modelvar_1950_1980[m] = var_i
-#     modelmeans_2070_2100[m] = means_f
-#     modelvar_2070_2100[m] = var_f
-#     mean_shifts[m] = np.array(means_f) - np.array(means_i)
-#     var_shifts[m] = np.array(var_f) - np.array(var_i)
-
-# basin_mean_shifts = {b: [] for b in basin_names} #dictionary indexed by basin name
-# basin_var_shifts = {b: [] for b in basin_names} #dictionary indexed by model name
-# basin_meanshift_meds = [] #arrays for plotting
-# basin_varshift_meds = []
-# basin_meanshift_range = []
-# basin_varshift_range = []
-
-# for i, b in enumerate(basin_names):
-#     bmeans_i = [np.nanmean(SPEI_by_model[m]['WRunoff'][i][600:971]) for m in modelnames]
-#     bvar_i = [np.nanvar(SPEI_by_model[m]['WRunoff'][i][600:971]) for m in modelnames]
-#     bmeans_f = [np.nanmean(SPEI_by_model[m]['WRunoff'][i][2039:2410]) for m in modelnames]
-#     bvar_f = [np.nanvar(SPEI_by_model[m]['WRunoff'][i][2039:2410]) for m in modelnames]
-#     basin_mean_shifts[b] = np.array(bmeans_f) - np.array(bmeans_i)
-#     basin_var_shifts[b] = np.array(bvar_f) - np.array(bvar_i)
-#     basin_meanshift_meds.append(np.nanmedian(basin_mean_shifts[b]))
-#     basin_varshift_meds.append(np.nanmedian(basin_var_shifts[b]))
-#     basin_meanshift_range.append(np.nanmax(basin_mean_shifts[b]) - np.nanmin(basin_mean_shifts[b]))
-#     basin_varshift_range.append(np.nanmax(basin_var_shifts[b]) - np.nanmin(basin_var_shifts[b]))
-
-
-# plt.figure('Mean and variance shifts, 2070-2100 versus 1950-1980, per basin for WRunoff case')
-# plt.errorbar(x=basin_meanshift_meds, y=basin_varshift_meds, xerr=basin_meanshift_range, yerr=basin_varshift_range, ls='')
-# plt.axes().set_xlabel('Difference in 30-yr mean SPEI', fontsize=16)
-# plt.axes().set_ylabel('Difference in SPEI variance', fontsize=16)
-# plt.axes().set_ylim(-0.5, 2.5)
-# plt.axes().set_xlim(-1, 5)
-# plt.show()
-
 
 # ## Calculate changes due to glacial effect at end of century, using gSPEI functions
 # bas_glac_meanmed, mean_spread = glacial_meandiff(SPEI_by_model)
