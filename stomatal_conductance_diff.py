0e21444f64b403218e9c307059cfb0d61a627f8f
Lizz Ultee [air] <ehultee@umich.edu>
2020-07-09 15:09:27 -0400

    Update plotting in stomatal_conductance_diff
    Correct filepath to saved data and make labels on plots bigger.

--- stomatal_conductance_diff.py
+++ stomatal_conductance_diff.py
@@ -13,7 +13,7 @@ import pandas as pd
 import gSPEI as gSPEI
 
 fpath_default = './data/SPEI_Files/'
-fpath_conduct = './data/SPEI_Files/variable_stom_conduct'
+fpath_conduct = './data/SPEI_Files/variable_stom_conduct/'
 
 ## Settings in filenames
 integration_times = np.arange(3, 28, 4) # all SPEI integration times used
@@ -76,9 +76,25 @@ for b in basin_names:
     vcd_mean_NR.append(df_diff.mean()) #mean difference across models for each basin
     vcd_perdiff_NR.append(perdiff.mean())
 
+## Plot histograms of difference
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+plt.rc('axes', titlesize=18, labelsize=16)
+
 fig, ax = plt.subplots(1)
+ax.axvline(0, ls=':', lw=3, color='Grey')
 ax.hist(vcd_perdiff, alpha=0.5)
-# ax.hist(vcd_perdiff_NR, alpha=0.5)
-ax.set(xlabel='Normalized difference due to stomatal conductance', ylabel='Count', 
-       title='SPEI glacial effect')
+ax.set(title='SPEI glacial effect (SPEI$_W$ - SPEI$_N$)',
+       xlabel='Normalized difference due to stomatal conductance', ylabel='Count',
+       ylim=(0,20), yticks=(0, 5, 10, 15, 20))
+plt.tight_layout()
+plt.show()
+
+fig1, ax1 = plt.subplots(1)
+ax1.axvline(0, ls=':', lw=3, color='Grey')
+ax1.hist(vcd_perdiff_NR, alpha=0.5)
+ax1.set(title='SPEI$_N$ series, no runoff',
+       xlabel='Normalized difference due to stomatal conductance', ylabel='Count', 
+       ylim=(0,40), yticks=(0, 10, 20, 30, 40))
+plt.tight_layout()
 plt.show()
