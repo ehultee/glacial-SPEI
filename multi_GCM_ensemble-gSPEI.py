9e4268281943681d774ceb604968f790c6d4b2ff
Lizz Ultee [air] <ehultee@umich.edu>
2021-05-18 18:02:15 -0400

    Support nonpara SPEI in ensembles
    Fill a few NaN values (on the order of 30 NaN months out of 2412) to allow computation of ensemble statistics from nonparametric SPEI values.

--- multi_GCM_ensemble-gSPEI.py
+++ multi_GCM_ensemble-gSPEI.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import gSPEI as gSPEI
 
-fpath = './data/SPEI_Files/variable_stom_conduct/'
+fpath = './data/SPEI_Files/nonparametric-var_stom_c/'
 
 ## Settings in filenames
 integration_times = np.arange(3, 28, 4) # all SPEI integration times used
@@ -33,14 +33,18 @@ yrs = np.linspace(1900, 2101, num=2412)
 ## Read all in to dict by GCM as in other gSPEI scripts
 SPEI_by_model = {m: {} for m in modelnames} # create dictionary indexed by model name
 for m in modelnames:
-    norunoff_f_m = fpath+'NRunoff_{}_{}_{}_Conduct.txt'.format(integration_times[0], m, scenarios[1])
-    wrunoff_f_m = fpath+'WRunoff_{}_{}_{}_Conduct.txt'.format(integration_times[0], m, scenarios[1])
+    norunoff_f_m = fpath+'NRunoff_{}_{}_{}_Conduct.txt'.format(integration_times[0], m, scenarios[0])
+    wrunoff_f_m = fpath+'WRunoff_{}_{}_{}_Conduct.txt'.format(integration_times[0], m, scenarios[0])
     SPEI_by_model[m]['NRunoff'] = np.loadtxt(norunoff_f_m)
     SPEI_by_model[m]['WRunoff'] = np.loadtxt(wrunoff_f_m)
     SPEI_by_model[m]['diff'] = SPEI_by_model[m]['WRunoff'] - SPEI_by_model[m]['NRunoff']
 
 ## Re-structure dictionary and create pandas DataFrames aggregated by basin
-SPEI_by_basin = gSPEI.sort_models_to_basins(SPEI_by_model)
+SPEI_by_basin_raw = gSPEI.sort_models_to_basins(SPEI_by_model)
+SPEI_by_basin = {b: {} for b in basin_names}
+for b in basin_names:
+    for c in cases:
+        SPEI_by_basin[b][c] = SPEI_by_basin_raw[b][c].fillna(method='bfill')
 
 ## Compute multi-GCM ensemble means and quartiles
 example_b = 'COPPER'
@@ -80,56 +84,56 @@ rm_q3_n = rm_n.quantile(q=0.75, axis=1)
 # plt.tight_layout()
 
 
-## Calculate changes due to glacial effect at end of century, using ensemble approach
-bas_glac_meandiff, quantile_spread = gSPEI.ensemble_glacial_meandiff(SPEI_by_basin)
-bas_glac_vardiff, var_spread = gSPEI.ensemble_glacial_vardiff(SPEI_by_basin)
-## Calculate median of mean shift and full multi-GCM range, for comparison
-bas_glac_meanmed, mean_spread_full = gSPEI.glacial_meandiff(SPEI_by_model)
-bas_glac_varmed, var_spread_full = gSPEI.glacial_vardiff(SPEI_by_model)
+# ## Calculate changes due to glacial effect at end of century, using ensemble approach
+# bas_glac_meandiff, quantile_spread = gSPEI.ensemble_glacial_meandiff(SPEI_by_basin)
+# bas_glac_vardiff, var_spread = gSPEI.ensemble_glacial_vardiff(SPEI_by_basin)
+# ## Calculate median of mean shift and full multi-GCM range, for comparison
+# bas_glac_meanmed, mean_spread_full = gSPEI.glacial_meandiff(SPEI_by_model)
+# bas_glac_varmed, var_spread_full = gSPEI.glacial_vardiff(SPEI_by_model)
 
-fig1, ax1 = plt.subplots(figsize=(5,4))
-ax1.axhline(y=0, ls=':', color='Grey', alpha=0.5)
-ax1.axvline(x=0, ls=':', color='Grey', alpha=0.5)
-ax1.errorbar(x=bas_glac_meandiff, y=bas_glac_vardiff, xerr=quantile_spread, yerr=var_spread, ls='', marker='d', elinewidth=2.0, color='DarkBlue')
-ax1.errorbar(x=bas_glac_meandiff, y=bas_glac_vardiff, xerr=mean_spread_full, yerr=var_spread_full, ls='', marker='d', elinewidth=1.0, color='DarkBlue', alpha=0.5) #extend whiskers to full range
-ax1.set_xlabel('Difference in mean SPEI', fontsize=16)
-ax1.set_ylabel('Difference in SPEI variance', fontsize=16)
-ax1.set(ylim=(-1.5, 1.0), xlim=(-0.5, 4))
-ax1.tick_params(axis='both', labelsize=12)
-plt.tight_layout()
-plt.show()
+# fig1, ax1 = plt.subplots(figsize=(5,4))
+# ax1.axhline(y=0, ls=':', color='Grey', alpha=0.5)
+# ax1.axvline(x=0, ls=':', color='Grey', alpha=0.5)
+# ax1.errorbar(x=bas_glac_meandiff, y=bas_glac_vardiff, xerr=quantile_spread, yerr=var_spread, ls='', marker='d', elinewidth=2.0, color='DarkBlue')
+# ax1.errorbar(x=bas_glac_meandiff, y=bas_glac_vardiff, xerr=mean_spread_full, yerr=var_spread_full, ls='', marker='d', elinewidth=1.0, color='DarkBlue', alpha=0.5) #extend whiskers to full range
+# ax1.set_xlabel('Difference in mean SPEI', fontsize=16)
+# ax1.set_ylabel('Difference in SPEI variance', fontsize=16)
+# ax1.set(ylim=(-1.5, 1.0), xlim=(-0.5, 4))
+# ax1.tick_params(axis='both', labelsize=12)
+# plt.tight_layout()
+# plt.show()
 
 
-# ## Four-basin example for manuscript
-# fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, 
-#                                               tight_layout=True, figsize=(9,6))
-# example_basins = ('COPPER', 'TARIM', 'RHONE', 'MAJES')
-# for example_b, ax in zip(example_basins, (ax1,ax2,ax3,ax4)):
-#     r_w = gSPEI.basin_ensemble_mean(SPEI_by_basin, example_b, 'WRunoff').rolling(window=12*30).mean()
-#     r_n = gSPEI.basin_ensemble_mean(SPEI_by_basin, example_b, 'NRunoff').rolling(window=12*30).mean()
-#     rm = SPEI_by_basin[example_b]['WRunoff'].rolling(window=12*30, axis=0).mean()
-#     rm_q1 = rm.quantile(q=0.25, axis=1)
-#     rm_q3 = rm.quantile(q=0.75, axis=1)
-#     rm_n = SPEI_by_basin[example_b]['NRunoff'].rolling(window=12*30, axis=0).mean()
-#     rm_q1_n = rm_n.quantile(q=0.25, axis=1)
-#     rm_q3_n = rm_n.quantile(q=0.75, axis=1)
+## Four-basin example for manuscript
+fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, 
+                                              tight_layout=True, figsize=(9,6))
+example_basins = ('COPPER', 'TARIM', 'RHONE', 'MAJES')
+for example_b, ax in zip(example_basins, (ax1,ax2,ax3,ax4)):
+    r_w = gSPEI.basin_ensemble_mean(SPEI_by_basin, example_b, 'WRunoff').rolling(window=12*30).mean()
+    r_n = gSPEI.basin_ensemble_mean(SPEI_by_basin, example_b, 'NRunoff').rolling(window=12*30).mean()
+    rm = SPEI_by_basin[example_b]['WRunoff'].rolling(window=12*30, axis=0).mean()
+    rm_q1 = rm.quantile(q=0.25, axis=1, interpolation='lower')
+    rm_q3 = rm.quantile(q=0.75, axis=1, interpolation='higher')
+    rm_n = SPEI_by_basin[example_b]['NRunoff'].rolling(window=12*30, axis=0).mean()
+    rm_q1_n = rm_n.quantile(q=0.25, axis=1, interpolation='lower')
+    rm_q3_n = rm_n.quantile(q=0.75, axis=1, interpolation='higher')
     
-#     ax.plot(yrs, r_w, 'k', linewidth=3.0)
-#     ax.plot(yrs, rm_q1, 'k')
-#     ax.plot(yrs, rm_q3, 'k')
-#     ax.plot(yrs, r_n, 'k', linewidth=3.0, ls=':')
-#     ax.plot(yrs, rm_q1_n, 'k', ls=':')
-#     ax.plot(yrs, rm_q3_n, 'k', ls=':')
-#     # for i in range(len(modelnames)):
-#     #     ax.plot(yrs, single_models_w[i], color=colors_w[i], alpha=0.5)
-#     #     ax.plot(yrs, single_models_n[i], color=colors_n[i], alpha=0.5)
-#     ax.fill_between(yrs, rm_q1, rm_q3, color='DarkBlue', alpha=0.2)
-#     ax.fill_between(yrs, rm_q1_n, rm_q3_n, color='DarkOrange', alpha=0.2)
-#     ax.tick_params(axis='both', labelsize=12)
-#     ax.set_xticks([2000, 2050, 2100])
-#     ax.set_xlim(1980, 2100)
-# for ax in (ax3, ax4):
-#     ax.set_xlabel('Year', fontsize=14)
-# for ax in (ax1, ax3):
-#     ax.set_ylabel('Rolling mean SPEI', fontsize=14)
-# fig1.align_ylabels()
+    ax.plot(yrs, r_w, 'k', linewidth=3.0)
+    ax.plot(yrs, rm_q1, 'k')
+    ax.plot(yrs, rm_q3, 'k')
+    ax.plot(yrs, r_n, 'k', linewidth=3.0, ls=':')
+    ax.plot(yrs, rm_q1_n, 'k', ls=':')
+    ax.plot(yrs, rm_q3_n, 'k', ls=':')
+    # for i in range(len(modelnames)):
+    #     ax.plot(yrs, single_models_w[i], color=colors_w[i], alpha=0.5)
+    #     ax.plot(yrs, single_models_n[i], color=colors_n[i], alpha=0.5)
+    ax.fill_between(yrs, rm_q1, rm_q3, color='DarkBlue', alpha=0.2)
+    ax.fill_between(yrs, rm_q1_n, rm_q3_n, color='DarkOrange', alpha=0.2)
+    ax.tick_params(axis='both', labelsize=12)
+    ax.set_xticks([2000, 2050, 2100])
+    ax.set_xlim(1980, 2100)
+for ax in (ax3, ax4):
+    ax.set_xlabel('Year', fontsize=14)
+for ax in (ax1, ax3):
+    ax.set_ylabel('Rolling mean SPEI', fontsize=14)
+fig1.align_ylabels()
